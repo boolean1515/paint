534ea0069f5b66e0068e47771f72686e24316bc8
Konstantin <boolean1515@gmail.com>
2013-02-28 20:55:12 +0700

    поехали =))

---
+++ рисовалка/scripts/script.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(go);
+
+function go() {
+    $("#tools1").draggable();
+    $("#tools2").draggable();
+    $("#tools3").draggable();
+    $("#tools4").draggable();
+}
+function start() {
+   
+    //добавить меню выбора 
+    var input = $('<div>', {
+        id: 'size'
+    });
+
+    // Помещаем в слой "content"
+    $('#centerLayer').append(input);
+
+    $("#centerLayer").hide();
+    $("#TB_overlay").hide();
+}
